b275efdf156ad1ea149b920abe1067fa1a994fe0
joseAcevesG <jose.aceves@iteso.mx>
2024-04-17 12:23:08 -0600

    fix: :construction_worker: fix for the eslint pre-commit
    file problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,12 @@
-prettier = require("eslint-config-prettier"); // Import the prettier plugin
+prettier = require("eslint-config-prettier");
 
 module.exports = {
-	files: ["**/*.js"], // Target JavaScript files
 	plugins: {
-		prettier: prettier, // Correctly configured plugin object
+		prettier: prettier,
 	},
 	rules: {
 		eqeqeq: ["error", "always"],
 		"no-empty-function": "error",
 		"no-unused-vars": "error",
 	},
-	ignores: [
-		"node_modules/**", // Ignore all files in the node_modules directory
-		"package.json", // Ignore package.json file
-		"package-lock.json", // Ignore package-lock.json file
-	],
 };
