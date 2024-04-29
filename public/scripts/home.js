4110de323378ad3a7dc5cad15ca4b852b6db600f
Salvador025 <jose.mariscal@iteso.mx>
2024-04-29 12:50:04 -0600

    fix: :zap: fixing prettier

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -11,28 +11,28 @@
 */
 
 // Espera a que el DOM esté completamente cargado antes de añadir el manejador de eventos
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtén el botón por su ID
-    var button = document.getElementById('nextPageButton');
+document.addEventListener("DOMContentLoaded", function () {
+	// Obtén el botón por su ID
+	var button = document.getElementById("nextPageButton");
 
-    // Añade un manejador de evento 'click' al botón
-    button.addEventListener('click', function() {
-        // Cambia la URL en la siguiente línea por la dirección a la que quieres redirigir
-        window.location.href = "/distance_race.html";
-    });
+	// Añade un manejador de evento 'click' al botón
+	button.addEventListener("click", function () {
+		// Cambia la URL en la siguiente línea por la dirección a la que quieres redirigir
+		window.location.href = "/distance_race.html";
+	});
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    var botonGuardar = document.getElementById('guardarNombre');
+document.addEventListener("DOMContentLoaded", function () {
+	var botonGuardar = document.getElementById("guardarNombre");
 
-    botonGuardar.addEventListener('click', function() {
-        // Obtiene el valor del input donde el usuario introduce su nombre
-        var nombre = document.getElementById('nombreUsuario').value;
+	botonGuardar.addEventListener("click", function () {
+		// Obtiene el valor del input donde el usuario introduce su nombre
+		var nombre = document.getElementById("nombreUsuario").value;
 
-        // Guarda el nombre en el localStorage
-        localStorage.setItem('nombre', nombre);
+		// Guarda el nombre en el localStorage
+		localStorage.setItem("nombre", nombre);
 
-        // Opcional: mostrar una confirmación o realizar alguna acción después de guardar
-        alert("Nombre guardado: " + nombre);
-    });
+		// Opcional: mostrar una confirmación o realizar alguna acción después de guardar
+		alert("Nombre guardado: " + nombre);
+	});
 });
