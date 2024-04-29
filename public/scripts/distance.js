e8658eda1258c435b8deee929783433fc41ccad1
Octavio Valdez <90404916+OctavioValdez@users.noreply.github.com>
2024-04-29 13:48:51 -0500

    feat: :zap: distance script
    sessionstorage variables

--- public/scripts/distance.js
+++ public/scripts/distance.js
@@ -4,18 +4,17 @@ document.getElementById("21k").addEventListener("click", set_21k);
 document.getElementById("42k").addEventListener("click", set_42k);
 
 function set_5k() {
-    sessionStorage.setItem("distance", "5k");
-};
+	sessionStorage.setItem("distance", "5k");
+}
 
 function set_10k() {
-    sessionStorage.setItem("distance", "5k");
-};
+	sessionStorage.setItem("distance", "5k");
+}
 
 function set_21k() {
-    sessionStorage.setItem("distance", "5k");
-};
+	sessionStorage.setItem("distance", "5k");
+}
 
 function set_42k() {
-    sessionStorage.setItem("distance", "5k");
-};
-
+	sessionStorage.setItem("distance", "5k");
+}
