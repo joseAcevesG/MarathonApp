95d188c4f05cf55235a3189e54680c5a065c0dac
Salvador025 <jose.mariscal@iteso.mx>
2024-04-29 11:35:02 -0600

    feat: :sparkles: Delete of the password

--- __tests__/routes/index.test.js
+++ __tests__/routes/index.test.js
@@ -1,19 +1,19 @@
-const request = require('supertest');
-const app = require('../app'); // Path to your main application file
+const request = require("supertest");
+const app = require("../app"); // Path to your main application file
 
-describe('Main Route Access', () => {
-    test('TC14: Main Route Accessible', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
-    });
+describe("Main Route Access", () => {
+	test("TC14: Main Route Accessible", async () => {
+		const response = await request(app).get("/");
+		expect(response.statusCode).toBe(200);
+	});
 
-    test('TC15: Undefined Routes', async () => {
-        const response = await request(app).get('/undefinedRoute');
-        expect(response.statusCode).toBe(404);
-    });
+	test("TC15: Undefined Routes", async () => {
+		const response = await request(app).get("/undefinedRoute");
+		expect(response.statusCode).toBe(404);
+	});
 
-    test('TC16: Redirects', async () => {
-        const response = await request(app).get('/old-route');
-        expect(response.statusCode).toBe(301); // Assuming 301 Moved Permanently
-    });
+	test("TC16: Redirects", async () => {
+		const response = await request(app).get("/old-route");
+		expect(response.statusCode).toBe(301); // Assuming 301 Moved Permanently
+	});
 });
--- __tests__/routes/users.test.js
+++ __tests__/routes/users.test.js
@@ -1,11 +1,11 @@
-const { registerUser, loginUser, recoverPassword } = require('./users');
-const userRoute = require('../../src/routes/users');
-const db = require('../../src/controllers/users.controller'); // Suponiendo que hay una dependencia de base de datos
-const bodyParser = require('body-parser');
+const { registerUser, loginUser, recoverPassword } = require("./users");
+const userRoute = require("../../src/routes/users");
+const db = require("../../src/controllers/users.controller"); // Suponiendo que hay una dependencia de base de datos
+const bodyParser = require("body-parser");
 
-jest.mock('../../src/controllers/users.controller');
+jest.mock("../../src/controllers/users.controller");
 
-const app = require('express')();
+const app = require("express")();
 app.use(bodyParser.json());
 app.use("/users", userRoute);
 
@@ -24,7 +24,7 @@ describe("User routes", () => {
                 name: 'John Doe',
                 email: 'jhon.doe@example.com',
                 username: 'johndoe',
-                password: 'Password123'
+                password: ''
             };
             
             const response = await request(app).post('/users/register').send(newUser);
@@ -34,28 +34,36 @@ describe("User routes", () => {
     }
 */
 
-describe('users.js', () => {
-  // TC1: Registro exitoso
-  test('should register a user successfully with all required fields', async () => {
-    const userData = { email: 'test@example.com', password: 'Password123', username: 'testuser' };
-    db.mockResolvedValue(true); // Simula una respuesta exitosa de la base de datos
-    const result = await registerUser(userData);
-    expect(result).toBeTruthy();
-  });
+describe("users.js", () => {
+	// TC1: Registro exitoso
+	test("should register a user successfully with all required fields", async () => {
+		const userData = {
+			email: "test@example.com",
+			password: "Password123",
+			username: "testuser",
+		};
+		db.mockResolvedValue(true); // Simula una respuesta exitosa de la base de datos
+		const result = await registerUser(userData);
+		expect(result).toBeTruthy();
+	});
 
-  // TC6: Autenticación exitosa
-  test('should authenticate a user with correct credentials', async () => {
-    const credentials = { email: 'test@example.com', password: 'Password123' };
-    db.findUser.mockResolvedValue({ id: 1, ...credentials }); // Simula encontrar al usuario en la base de datos
-    const result = await loginUser(credentials);
-    expect(result.isAuthenticated).toBeTruthy();
-  });
+	// TC6: Autenticación exitosa
+	test("should authenticate a user with correct credentials", async () => {
+		const credentials = { email: "test@example.com", password: "Password123" };
+		db.findUser.mockResolvedValue({ id: 1, ...credentials }); // Simula encontrar al usuario en la base de datos
+		const result = await loginUser(credentials);
+		expect(result.isAuthenticated).toBeTruthy();
+	});
 
-  // TC9: Recuperación de contraseña
-  test('should handle password recovery flow', async () => {
-    const email = 'test@example.com';
-    db.findUserByEmail.mockResolvedValue({ id: 1, email, resetToken: 'token123' }); // Simula encontrar al usuario y generar un token
-    const result = await recoverPassword(email);
-    expect(result).toHaveProperty('resetToken');
-  });
+	// TC9: Recuperación de contraseña
+	test("should handle password recovery flow", async () => {
+		const email = "test@example.com";
+		db.findUserByEmail.mockResolvedValue({
+			id: 1,
+			email,
+			resetToken: "token123",
+		}); // Simula encontrar al usuario y generar un token
+		const result = await recoverPassword(email);
+		expect(result).toHaveProperty("resetToken");
+	});
 });
